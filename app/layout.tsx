b72809199bb1ce5375a8c83150a5e99a9b07b5e2
thainapires <thainaspiress@gmail.com>
2026-09-06 20:55:23 -0300

    fix: add supress warning to not get error

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="pt-BR">
-            <body>{children}</body>
+            <body suppressHydrationWarning>{children}</body>
         </html>
     );
 }
